cefc5c22e06aad211e478ae2744f550f998b154e
Ananyaaa004 <154814764+Ananyaaa004@users.noreply.github.com>
2024-01-19 07:06:18 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,4 +1,4 @@
-# pcos_detection_model.py
+
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -12,7 +12,6 @@ def load_data():
     df = pd.read_excel(r"C:\Users\ananya\Downloads\PCOS_data.xlsx", sheet_name="Full_new")
 
 
-    # Preprocess the data (you may replace this with your actual preprocessing steps)
     df['II    beta-HCG(mIU/mL)'] = pd.to_numeric(df['II    beta-HCG(mIU/mL)'], errors='coerce')
     df['AMH(ng/mL)'] = pd.to_numeric(df['AMH(ng/mL)'], errors='coerce')
     df.drop('Unnamed: 44', axis=1, inplace=True)
@@ -28,17 +27,13 @@ def load_data():
 
     return df_new
 def train_model(df):
-    # Define features (X) and target variable (y)
     X = df.drop(['PCOS (Y/N)'], axis=1)
     y = df['PCOS (Y/N)']
 
-    # Split the dataset into training and testing sets
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Initialize logistic regression model
     logreg = LogisticRegression(max_iter=1000)  
 
-    # Train the model
     logreg.fit(x_train, y_train)
 
     return logreg
